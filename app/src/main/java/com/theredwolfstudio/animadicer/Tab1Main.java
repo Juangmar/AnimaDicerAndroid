d4ce91ce61a69698096995a74ac0f37e4477d11d
KoldurJ <jugomezm@ucm.es>
2017-12-24 00:15:16 +0100

    Progress bar actualizadas Pruebas con la personalización de las barras de progreso

--- app/src/main/java/com/theredwolfstudio/animadicer/Tab1Main.java
+++ app/src/main/java/com/theredwolfstudio/animadicer/Tab1Main.java
@@ -3,13 +3,15 @@ package com.theredwolfstudio.animadicer;
 /**
  * Created by Koldur on 15/12/2017.
  */
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 public class Tab1Main extends Fragment{
 
@@ -17,6 +19,20 @@ public class Tab1Main extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab1main, container, false);
+        // Get the Drawable custom_progressbar
+        Drawable life= rootView.getResources().getDrawable(R.drawable.customprogressbar_life);
+        Drawable zeon= rootView.getResources().getDrawable(R.drawable.customprogressbar_zeon);
+        Drawable ki= rootView.getResources().getDrawable(R.drawable.customprogressbar_ki);
+        Drawable tir= rootView.getResources().getDrawable(R.drawable.customprogressbar_tired);
+        // set the drawable as progress drawable
+        ProgressBar vida = rootView.findViewById(R.id.progressBar5LIFE);
+        vida.setProgressDrawable(life);
+        ProgressBar elki = rootView.findViewById(R.id.progressBarKi);
+        elki.setProgressDrawable(ki);
+        ProgressBar zen = rootView.findViewById(R.id.progressBarZeon);
+        zen.setProgressDrawable(zeon);
+        ProgressBar tired = rootView.findViewById(R.id.progressBarCans);
+        tired.setProgressDrawable(tir);
         return rootView;
     }
 }
